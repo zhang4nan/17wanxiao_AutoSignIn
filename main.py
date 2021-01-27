245f07b51dc17c2d3c9b85461cf3c8a02f289817
llkhs <llkhs@qq.com>
2021-01-27 18:15:18 +0800

    fixup! 😁修复了会话失效和推送的问题！！！

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ def main():
         print("-----------------------")
     fail = sorted(set(failure), key=failure.index)
     title = "成功: %s 人,失败: %s 人" % (len(success), len(fail))
+    if (fail is None):
+       fail = ['我见青山多妩媚']
+       fail = ''.join(fail)
     for _ in range(1):
         try:
             if not (sckey is None) & run:
@@ -125,9 +128,6 @@ def check_in(token):
 def WechatPush(title, sckey, success, fail, result):
     send_url = f"https://sc.ftqq.com/{sckey}.send"
     strTime = GetNowTime()
-    if fail is None:
-       fail = ["我见青山多妩媚"]
-       fail = ''.join(fail)
     page = json.dumps(result, sort_keys=True, indent=4, separators=(',', ':'), ensure_ascii=False)
     content = [f"""`{strTime}`
 #### 打卡成功用户:
